deb793ae900a505288c8d7a9503fabe0b7c9ce30
StrukovSergei <strukov.sergei.999@gmail.com>
2023-11-20 17:36:59 +0200

    header links

--- frontend/src/components/AppHeader.tsx
+++ frontend/src/components/AppHeader.tsx
@@ -1,17 +1,25 @@
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const AppHeader = () => {
-    const navigate = useNavigate();
 
-    const handleHeaderClick = () => {
-        // Navigate to the root when the header is clicked
-        navigate('/');
-    };
 
     return (
-        <h1 onClick={handleHeaderClick} style={{ cursor: 'pointer' }}>
-            App header
-        </h1>
+        <>
+            <Link to={`/`}>
+                <div className="header">
+                    <div className="title">Recipe4Me</div>
+                </div>
+            </Link>
+            <Link to={`/search`}>
+                <a>Search</a>
+            </Link>
+            <Link to={`/favourites`}>
+                <a>Favourites</a>
+            </Link>
+            <Link to={`/about`}>
+                <a>About</a>
+            </Link>
+        </>
     );
 };
 
--- frontend/src/pages/AppHome.tsx
+++ frontend/src/pages/AppHome.tsx
@@ -14,20 +14,7 @@ const AppHome = () => {
   return (
     <>
       <AppHeader />
-      <div className="app-container">
-        <div className="header">
-          <img src="/hero-image.jpg" alt="Hero"></img>
-          <div className="title">Recipe4Me</div>
-        </div>
-        <div className="tabs">
-          <Link to="/search" className={selectedTab === "search" ? "tab-active" : ""}>
-            <h1>Recipe Search</h1>
-          </Link>
-          <Link to="/favourites" className={selectedTab === "favourites" ? "tab-active" : ""}>
-            <h1>Favourites</h1>
-          </Link>
-        </div>
-      </div>
+
     </>
   )
 };
