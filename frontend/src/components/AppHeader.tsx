6386f83781b73fb0f8e2d69a6b59933ab09d618d
StrukovSergei <strukov.sergei.999@gmail.com>
2023-11-21 22:26:20 +0200

    tailwind configuration + basic header

--- frontend/src/components/AppHeader.tsx
+++ frontend/src/components/AppHeader.tsx
@@ -1,26 +1,38 @@
 import { Link } from 'react-router-dom';
 
 const AppHeader = () => {
-
-
     return (
-        <>
-            <Link to={`/`}>
-                <div className="header">
-                    <div className="title">Recipe4Me</div>
-                </div>
+        <div className="flex justify-between items-center p-4 bg-blue-500 text-white">
+            {/* Logo on the left */}
+            <Link to={`/`} className="text-lg font-bold">
+                Recipe4Me
             </Link>
-            <Link to={`/search`}>
-                <a>Search</a>
-            </Link>
-            <Link to={`/favourites`}>
-                <a>Favourites</a>
-            </Link>
-            <Link to={`/about`}>
-                <a>About</a>
-            </Link>
-        </>
+
+            {/* Tabs in the middle */}
+            <div className="flex space-x-4">
+                <Link to={`/search`} className="hover:text-gray-300">
+                    Search
+                </Link>
+                <Link to={`/favourites`} className="hover:text-gray-300">
+                    Favourites
+                </Link>
+                <Link to={`/about`} className="hover:text-gray-300">
+                    About
+                </Link>
+            </div>
+
+            {/* Social links on the right */}
+            <div className="flex space-x-4">
+                <a href="#" className="hover:text-gray-300">
+                    {/* Replace with your social icon (e.g., Twitter, Facebook, etc.) */}
+                    <i className="fab fa-twitter"></i>
+                </a>
+                <a href="#" className="hover:text-gray-300">
+                    {/* Replace with your social icon */}
+                    <i className="fab fa-facebook"></i>
+                </a>
+            </div>
+        </div>
     );
 };
-
 export default AppHeader;
