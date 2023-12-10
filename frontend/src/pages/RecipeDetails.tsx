051c1eb64689a5b4036e966b122714c88a648495
StrukovSergei <strukov.sergei.999@gmail.com>
2023-12-10 20:18:49 +0200

    basic details style

--- frontend/src/pages/RecipeDetails.tsx
+++ frontend/src/pages/RecipeDetails.tsx
@@ -28,41 +28,42 @@ const RecipeDetails = () => {
     console.log(recipeInfo)
 
     return (
-        <>
+        <div className="max-w-3xl mx-auto mt-8 p-4 bg-white rounded shadow-md">
             <AppHeader />
-            <div>
-                <h2>{recipeInfo.title}</h2>
-                <p>Recipe ID: {recipeInfo.id}</p>
-                <p>Ready in minutes: {recipeInfo.readyInMinutes}</p>
-                <p>Servings: {recipeInfo.servings}</p>
-                <p>Vegan: {recipeInfo.vegan ? 'Yes' : 'No'}</p>
-                <p>Gluten-Free: {recipeInfo.glutenFree ? 'Yes' : 'No'}</p>
-                <p>Health Score: {recipeInfo.healthScore}</p>
-                <img src={recipeInfo.image} alt={recipeInfo.title} />
-                <h3>Ingredients:</h3>
-                <ul>
-                    {recipeInfo.extendedIngredients.map((ingredient, index) => (
-                        <li key={index}>
-                            {ingredient.name}: {ingredient.amount} {ingredient.unit}
-                        </li>
-                    ))}
-                </ul>
-
-                <h3>Instructions:</h3>
-                <ul>
-                    {recipeInfo.analyzedInstructions.map((instruction, index) => (
-
-
-                        <ol>
-                            {instruction.steps.map((step, stepIndex) => (
-                                <li key={stepIndex}>{step.step}</li>
-                            ))}
-                        </ol>
-
-                    ))}
-                </ul>
+            <h2 className="text-3xl font-bold mb-4">{recipeInfo.title}</h2>
+            <div className="grid grid-cols-2 gap-4 mb-4">
+                <div>
+                    <img src={recipeInfo.image} alt={recipeInfo.title} className="w-full h-auto rounded" />
+                </div>
+                <div>
+                    <p><strong>Recipe ID:</strong> {recipeInfo.id}</p>
+                    <p><strong>Ready in minutes:</strong> {recipeInfo.readyInMinutes}</p>
+                    <p><strong>Servings:</strong> {recipeInfo.servings}</p>
+                    <p><strong>Vegan:</strong> {recipeInfo.vegan ? 'Yes' : 'No'}</p>
+                    <p><strong>Gluten-Free:</strong> {recipeInfo.glutenFree ? 'Yes' : 'No'}</p>
+                    <p><strong>Health Score:</strong> {recipeInfo.healthScore}</p>
+                </div>
             </div>
-        </>
+            <h3 className="text-xl font-bold mb-2">Ingredients:</h3>
+            <ul>
+                {recipeInfo.extendedIngredients.map((ingredient, index) => (
+                    <li key={index} className="mb-1">
+                        {ingredient.name}: {ingredient.amount} {ingredient.unit}
+                    </li>
+                ))}
+            </ul>
+
+            <h3 className="text-xl font-bold mt-4 mb-2">Instructions:</h3>
+            <ol className="list-decimal list-inside">
+                {recipeInfo.analyzedInstructions.map((instruction, index) => (
+                    <li key={index}>
+                        {instruction.steps.map((step, stepIndex) => (
+                            <p key={stepIndex}>{step.step}</p>
+                        ))}
+                    </li>
+                ))}
+            </ol>
+        </div>
     );
 };
 
